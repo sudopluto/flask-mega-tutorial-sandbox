851d0f0e809c0f39a826f1d3673dea839445d3c3
Pranav Sharma <pranav.sharma.ama@gmail.com>
2022-09-15 23:17:40 -0400

    done with chapter 2

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return "Hello World"
+    user = {'username': 'Pranav'}
+    posts = [
+        {
+            'author': {'username': 'Johnny'},
+            'body': 'I like beer!'
+        },
+        {
+            'author': {'username': 'Danny'},
+            'body': 'I like bonsais!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
